def87d3cc7cf1726e589c7335e6e20afcbb33d3d
Anja <anja_bukovec@t-2.net>
2017-04-28 18:25:22 +0200

    GOT views

---
+++ game_of_thrones.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from urllib2 import urlopen
+from BeautifulSoup import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/Game_of_Thrones"
+
+open_got = urlopen(url).read()
+
+soup = BeautifulSoup(open_got)
+
+wikitable = soup.find("table", {"class": "wikitable"})
+
+views = 0
+
+for link in wikitable.findAll('a'):
+    if "Season 7" in link.string:
+        pass
+    elif "Season" in link.string:
+        season_link = "https://en.wikipedia.org" + link["href"]
+        open_season_link = urlopen(season_link).read()
+        soup2 = BeautifulSoup(open_season_link)
+
+        wikitable2 = soup2.find("table", {"class": "wikitable plainrowheaders wikiepisodetable"})
+
+        for row in wikitable2.findAll("tr")[1:]:  #excludes first row of the table
+            for td in row.findAll("td")[5:]:    #takes only the last column
+                #print td.text[:4]
+                views += float(td.text[:4])  #takes only first four numbers
+
+print (str(views) + " millions")
